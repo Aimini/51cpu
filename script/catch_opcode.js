454f2c35aa9bb501d29bbd13535261c45384566e
Aimini <2026084033@qq.com>
2019-02-27 22:56:31 +0800

    can get hex to memnoric map now

--- script/catch_opcode.js
+++ script/catch_opcode.js
@@ -18,17 +18,22 @@ function get_op_code(t) {
 
 function print_opcode(t) {
     let text = ''
+    let bin_name = ''
     t.forEach(function (v) {
         let [s0, s1, s2, s3] = v;
+        bin_name += (`0x${s0}:'${s2}',`) //add element to dict
         s0 += ',   '
         s1 += ',   '
         s2 += ','
-        while (s2.length < 10) {
-            s2 += ' '
+        let space = ''
+        while (s2.length + space.length < 10) {
+            space += ' '
         }
-        text += ('#' + s0 + s1 + s2 + s3 + '\n[],\n')
+       
+        text += (`#${s0},   ${s1},   ${s2},${space}${s3}`+'\n[],\n')
     })
     console.warn(text)
+    console.warn(`HEX_TO_BIN = {${bin_name}}`)
 }
 
 
