4e55d2531a6c070e5bc64bcf0abec45ee4c182f9
Aimini <2026084033@qq.com>
2019-02-28 00:15:24 +0800

    rename instruction.py to InstructionGenerate.py; Reformat InterruptCycle.py

---
+++ script/InstructionGenrate.py
@@ -0,0 +1,60 @@
+import pathlib,math
+import instructions.implement as i_impl
+import instructions.info as i_info
+
+oscillation_cycle = 12
+oscillation_cycle_max = 24
+final_instructions = [x for x in i_impl.INSTRUCTIONS]
+
+# ---------------------------------- RETI,
+ignore_interrupt_check_instruction = [0x32, ]
+
+
+def add_fetch(instructions):
+    for one in instructions:
+        one.insert(0, ['PC_O', 'PAR_I'])
+        one.insert(1, ['PC_INC', 'FLASH_O', 'IR_I'])
+
+
+def add_recount_and_interrupt_check(instructions):
+    for idx, one in enumerate(instructions):
+        # check instruction's oscil cycle len
+        instruction_len = len(one)
+        if instruction_len > oscillation_cycle_max:
+            print("The instruction {}(0x{:0>2x})"
+                  " uses an excessively long oscillation period".format(i_info.HEX_TO_NAME[idx], idx))
+        add_where = math.ceil(instruction_len/oscillation_cycle)*oscillation_cycle
+        one.extend([[] for __ in range(add_where - instruction_len)])
+        one[add_where - 1].append('MIC_CLR')
+
+        if idx in ignore_interrupt_check_instruction:
+            print("{}(0x{:0>2x}) ignore interrupt check.".format(i_info.HEX_TO_NAME[idx], idx))
+        else:
+            one[add_where - 1].append('INT_CHK')
+
+
+directory = pathlib.Path("../eeprom-bin")
+
+
+def write_as_bin(number8bit):
+    return (number8bit).to_bytes(length=1, byteorder='big')
+
+
+def write_to_file(file, write_func):
+    pass
+
+
+def print_instructions(instructions):
+    for x in instructions:
+        print(x)
+
+def write_to_bin():
+    # file = open(directory / "instructions.bin",'bw')
+    # write_to_file(file,write_as_bin)
+    # file.close()
+    add_fetch(final_instructions)
+    add_recount_and_interrupt_check(final_instructions)
+    print_instructions(final_instructions)
+
+
+write_to_bin()
--- script/InterruptCycle.py
+++ script/InterruptCycle.py
@@ -1,75 +1,69 @@
 import pathlib
-import math 
+import math
 from functools import reduce
 
-#label maybe use
-MI_LABEL = {"PC_O","PC_I","BUFFER_I","BUFFER_L8O","BUFFER_H8O","SP_O","SP_A1","MEM_I","MAR_I","MIC_AR","IC_F_AR","INT_VEC_O"}
+# label maybe use
+MI_LABEL = {"PC_O", "PC_I", "BUFFER_I", "BUFFER_L8O", "BUFFER_H8O", "SP_O", "SP_A1", "MEM_I", "MAR_I", "MIC_AR",
+            "IC_F_AR", "INT_VEC_O"}
 # record used label
 USED_MI_LABEL = {}
 # Label corresponding binary
-MI_LABEL_BIN_MAP = {v:1 << i for i,v in enumerate(MI_LABEL)}
-#Microinstruction that takes place at each step of the interrupt cycle
+MI_LABEL_BIN_MAP = {v: 1 << i for i, v in enumerate(MI_LABEL)}
+# Micro instruction that takes place at each step of the interrupt cycle
 INSTRUCTIONS = [
-    ["PC_O","BUFFER_I","SP_O","MAR_I"],
-    ["BUFFER_L8O","MEM_I","SP_A1"],
-    ["SP_O","MAR_I"],
-    ["BUFFER_H8O","MEM_I","SP_A1","INT_VEC_O","PC_I","MIC_AR","IC_F_AR"],
+    ["PC_O", "BUFFER_I", "SP_O", "MAR_I"],
+    ["BUFFER_L8O", "MEM_I", "SP_A1"],
+    ["SP_O", "MAR_I"],
+    ["BUFFER_H8O", "MEM_I", "SP_A1", "INT_VEC_O", "PC_I", "MIC_AR", "IC_F_AR"],
 ]
 
-
-
-
-
-
-
-directory =  pathlib.Path("../eeprom-bin")
+directory = pathlib.Path("../eeprom-bin")
 file_prefix = "int-cycle"
+
 
 # covert a 8bit number to byte
 def write_as_bin(number8bit):
-   return (number8bit).to_bytes(length=1, byteorder='big')
+    return (number8bit).to_bytes(length=1, byteorder='big')
 
 
-def create_instruction_bin(instructions,mi_label_to_bin):
+def create_instruction_bin(instructions, mi_label_to_bin):
     inss_bin = []
     used_mi = set()
-    for one in instructions: # get each step
+    for one in instructions:  # get each step
         bin_code = 0
-        for mi_label in one: # get each microinstruction
-            b = mi_label_to_bin.get(mi_label,None) # get microinstructon's binary code
-            if b is not None: 
-                bin_code |= b #if find binary code in map,append it to instruction binary code
+        for mi_label in one:  # get each microinstruction
+            b = mi_label_to_bin.get(mi_label, None)  # get microinstructon's binary code
+            if b is not None:
+                bin_code |= b  # if find binary code in map,append it to instruction binary code
             else:
                 print("unknow mi label:" + mi_label)
                 exit(-1)
-            used_mi.add(mi_label) # add used microinstruction
+            used_mi.add(mi_label)  # add used microinstruction
         inss_bin.append(bin_code)
-    return inss_bin,used_mi
+    return inss_bin, used_mi
 
 
 def print_mi_map(mi_bin_map):
     l = len(mi_bin_map)
-    s = '{:0>'+ str(l) + 'b}' #fill 0 ,aligin right
-    for k,v in mi_bin_map.items():
-        print(("{:<12}" + s).format(k,v))
+    s = '{:0>' + str(l) + 'b}'  # fill 0 ,aligin right
+    for k, v in mi_bin_map.items():
+        print(("{:<12}" + s).format(k, v))
+
 
 def write_to_file():
-    instructions_bin,used_mi = create_instruction_bin(INSTRUCTIONS,MI_LABEL_BIN_MAP)
+    instructions_bin, used_mi = create_instruction_bin(INSTRUCTIONS, MI_LABEL_BIN_MAP)
     unused_mi = MI_LABEL.difference(used_mi)
     if len(unused_mi) != 0:
         print("unused micro instruction LABEL:")
         print(",".join(unused_mi))
-        
-    for x in range(math.ceil(len(MI_LABEL)/8)):#检查用到几个微指令需要几个eeprom，一个eeprom可以控制8个微指令
-        file = open(directory / (file_prefix+"-"+str(x)+".bin"),'bw')
+
+    for x in range(math.ceil(len(MI_LABEL) / 8)):  # 检查用到几个微指令需要几个eeprom，一个eeprom可以控制8个微指令
+        file = open(directory / (file_prefix + "-" + str(x) + ".bin"), 'bw')
         for i in instructions_bin:
-            file.write(write_as_bin( 0xFF &(i >> (x*8))))
+            file.write(write_as_bin(0xFF & (i >> (x * 8))))
         file.close()
 
     print_mi_map(MI_LABEL_BIN_MAP)
-            
-        
 
-    
 
 write_to_file()
