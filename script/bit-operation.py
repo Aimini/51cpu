5c10f15c84cfe76f665350df8ab7e6b9b2ae30f4
Aimini <2026084033@qq.com>
2019-04-08 03:54:34 +0800

    opt: merge CLR_CY to eeprom

--- script/bit-operation.py
+++ script/bit-operation.py
@@ -3,18 +3,21 @@ import inuntil
 
 directory = pathlib.Path("eeprom-bin")
 def bitoperation_conuter(doit):
-    for bit_in in range(2**1):
-            for bs in range(2**3):
-                for data in range(2**8):
-                    doit(bit_in,bs,data)
+    for clr_cy in range(2**1):
+        for bit_in in range(2**1):
+                for bs in range(2**3):
+                    for data in range(2**8):
+                        doit(clr_cy,bit_in,bs,data)
 
 
 
 def bitop_gen(f):
-    def gen(bit_in,bit_select,data):
+    def gen(clr_cy,bit_in,bit_select,data):
         mask = (1 << bit_select)
         clear_bit = data & (0xFF&(~mask))
         result = clear_bit | (bit_in << bit_select)
+        if clr_cy == 1:
+            result &= 0x7F
         print('{} {} {:0>8b} {:0>8b}'.format(bit_in,bit_select,data,result))
         f.write(inuntil.write_as_bin(result))
     return gen
